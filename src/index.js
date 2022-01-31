54e73106fe51ae4a13080d9fab40d8b9c914fdd1
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-01-31 14:20:28 +0000

    Add task and project factories

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import project from "./modules/projects";
+import task from "./modules/tasks";
+
+let p1 = project('pro1');
+console.log(p1);
---
+++ src/modules/tasks.js
@@ -0,0 +1,57 @@
+function task(name, dueDate) {
+    let priority = 'medium';
+    let subTasks = [];
+    let complete = false;
+ 
+    // Get attributes
+    const getName = () => name;
+    const getDueDate = () => dueDate;
+    const getPriority = () => priority;
+    const getSubTasks = () => subTasks;
+    const getComplete = () => complete;
+
+    // Edit project attributes
+    const setName = (newName) => {
+        name = newName;
+    }
+
+    const setDueDate = (newDueDate) => {
+        dueDate = newDueDate;
+    }
+
+    const setPriority = (newPriority) => {
+        priority = newPriority;
+    }
+
+    // Add sub task
+    const addSubTask = (newSubTask) => {
+        subTasks.push(newSubTask);
+    }
+
+    // Delete sub task
+    const deleteSubTask = (subTask) => {
+        const index = subTasks.indexOf(subTask);
+        subTasks.splice(index, 1);
+    }
+
+    // Toggle complete
+    const toggleComplete = () => {
+        complete = !complete;
+    }
+
+    return {
+        getName,
+        setName,
+        getDueDate,
+        setDueDate,
+        getPriority,
+        setPriority,
+        getSubTasks,
+        addSubTask,
+        deleteSubTask,
+        getComplete,
+        toggleComplete,
+    }
+}
+
+export default task;
